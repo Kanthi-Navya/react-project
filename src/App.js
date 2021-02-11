2fd7acdfa2c47ff94487c8a56278116ebce1451b
navyarani-k <navyarani.k@etvbharat.com>
2021-02-11 11:43:33 +0530

    components part-2

--- src/App.js
+++ src/App.js
@@ -1,19 +1,32 @@
 import logo from './logo.svg';
+import React, { Component } from "react"
 import { Navbar, NavbarBrand } from 'reactstrap';
 import Menu from './components/MenuComponent';
 import './App.css';
+import { DISHES } from './shared/dishes';
+import { constructor } from '@babel/types';
+// function App() {
+class App extends Component {
+  constructor(props) 
+  {
+    super(props);
 
-function App() {
-  return (
+    this.state = {
+      dishes: DISHES
+    };
+  }
+  render() {
+  return(
     <div>
       <Navbar dark color= "primary">
         <div className="container">
           <NavbarBrand href = "/">Ristorante Con Fusion</NavbarBrand>
         </div>
       </Navbar>  
-      <Menu/>
+      <Menu dishes={this.state.dishes}/>
     </div>
   );
+  }
 }
 
 export default App;
